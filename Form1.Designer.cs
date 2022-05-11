7ca65aab676f882a5dc6db0d832fc9c22fe55930
Muhammed Fener <sonkale29@gmail.com>
2022-05-11 03:17:28 +0300

    Gereksiz kutular kaldırıldı.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -66,8 +66,6 @@ namespace Teklif_Listesi_Hazırlama_Modülü
             this.urunsil = new System.Windows.Forms.Button();
             this.veritabaniformac = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -400,27 +398,11 @@ namespace Teklif_Listesi_Hazırlama_Modülü
             this.saveFileDialog1.Filter = "PDF|*.pdf";
             this.saveFileDialog1.Title = "Dosya Oluştur";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(673, 291);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 21;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(673, 317);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 21;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(861, 519);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.veritabaniformac);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
@@ -485,8 +467,6 @@ namespace Teklif_Listesi_Hazırlama_Modülü
         private System.Windows.Forms.Button urunsil;
         private System.Windows.Forms.Button veritabaniformac;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
